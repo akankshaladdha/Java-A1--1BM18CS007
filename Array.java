79f3aea9ee02c21db3df9cea21944b7234002990
akankshaladdha <53806569+akankshaladdha@users.noreply.github.com>
2019-08-14 20:31:11 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Array
+
+{
+    
+	public static void main(String args[])
+    
+{
+       Scanner se=new Scanner(System.in);
+        
+	int size,n,i;
+        
+	System.out.println("Enter size of array");
+        
+	size=se.nextInt();
+        
+	int arr[] = new int[size];
+        
+	System.out.println("Enter elements of array");
+        
+	int sum=0;
+        
+	for(i=0;i<size;i++)
+        
+	{
+            
+		arr[i]=se.nextInt();
+            
+		sum+=arr[i];
+        
+	}
+        
+	System.out.println("Sum of elements is :  "+ sum);
+        
+        
+   
+}
+
+}
